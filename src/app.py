5d1499dbd5d599c0be300510d88d480f48287e42
RianMehta21 <rianmehta20@gmail.com>
2025-06-16 20:56:16 +0800

    ui error update

--- src/app.py
+++ src/app.py
@@ -1,37 +1,76 @@
 """"The GUI for main.py"""
 import tkinter as tk
 from tkinter import StringVar, ttk
+import data_base
 
-root = tk.Tk()
-root.geometry("1000x500")
-root.title("Stock Tracker")
 
-input_frame = tk.Frame(root)
-input_frame.pack()
-for i in range(5):
-    input_frame.columnconfigure(i, weight = 1)
+class MyGUI:
+    """Contains all GUI related functions"""
 
-header = tk.Label(input_frame, text = "Ticker")
-header.grid(row = 0, column = 1)
-header = tk.Label(input_frame, text = "Quantity")
-header.grid(row = 0, column = 2)
-header = tk.Label(input_frame, text = "Price")
-header.grid(row = 0, column = 3)
+    def __innit__(self):
+        """Creates the GUI window"""
+        self.root = tk.Tk()
+        self.root.geometry("1000x500")
+        self.root.title("Stock Tracker")
 
-options = ["BUY", "BUY", "SHORT", "SELL"]
-drop_down = ttk.OptionMenu(input_frame, StringVar(value = "BUY"), *options)
-drop_down.grid(row = 1, column = 0)
+        self.input_frame = tk.Frame(self.root, pady=100)
+        self.input_frame.pack()
+        for i in range(5):
+            self.input_frame.columnconfigure(i, weight=1)
 
-ticker_input = ttk.Entry(input_frame)
-ticker_input.grid(row=1, column=1)
+        self.header = tk.Label(self.input_frame, text="Ticker")
+        self.header.grid(row=0, column=1)
+        self.header = tk.Label(self.input_frame, text="Quantity")
+        self.header.grid(row=0, column=2)
+        self.header = tk.Label(self.input_frame, text="Price")
+        self.header.grid(row=0, column=3)
 
-quantity_input = ttk.Entry(input_frame)
-quantity_input.grid(row=1, column=2)
+        options = ["BUY", "SHORT", "SELL"]
+        self.type = StringVar()
+        self.type.set(options[0])
+        self.drop_down = tk.OptionMenu(self.input_frame, self.type, *options)
+        self.drop_down.grid(row=1, column=0)
 
-price_input = ttk.Entry(input_frame)
-price_input.grid(row=1, column=3)
+        self.ticker_input = ttk.Entry(self.input_frame)
+        self.ticker_input.grid(row=1, column=1)
 
-submit = ttk.Button(input_frame, text = "Submit")
-submit.grid(row=1, column=4)
+        self.quantity_input = ttk.Entry(self.input_frame)
+        self.quantity_input.grid(row=1, column=2)
 
-root.mainloop()
+        self.price_input = ttk.Entry(self.input_frame)
+        self.price_input.grid(row=1, column=3)
+
+        self.submit = ttk.Button(self.input_frame, text="Submit", command=self.submit)
+        self.submit.grid(row=1, column=4)
+
+        self.root.mainloop()
+
+    def submit(self):
+        """Submits the input in the GUI to the backend"""
+
+        for widget in self.input_frame.grid_slaves():
+            if widget.grid_info()["row"] == 2:
+                widget.destroy()
+
+        quantity = self.quantity_input.get()
+        ticker = self.ticker_input.get()
+        type = self.type.get()
+        price = self.price_input.get()
+
+        if not all([quantity, ticker, type, price]):
+            tk.Label(self.input_frame, text="Fill all the values", fg = "red", font=('Helvetica', 12)).grid(row=2, column=2)
+        else:
+            try:
+                quantity = int(quantity)
+            except ValueError:
+                tk.Label(self.input_frame, text="Invalid quantity input", fg = "red", font=('Helvetica', 12)).grid(row=2, column=2)
+                return
+            try:
+                price = float(price)
+            except ValueError:
+                tk.Label(self.input_frame, text="Invalid price input", fg = "red", font=('Helvetica', 12)).grid(row=2, column=2)
+                return
+
+
+gui = MyGUI()
+gui.__innit__()
